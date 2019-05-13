8e2e49e07477d33719146b40caa6deebfcc4735b
ASEBASTIAN <sebastian.axinte@cst-dev.com>
2019-05-13 17:57:00 +0300

    Product quantity validation and fixes

--- src/components/product-quantity/product-quantity.ts
+++ src/components/product-quantity/product-quantity.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef} from '@angular/core';
 import {Product} from "../../interfaces/models/product";
 import {ProgramProvider} from "../../providers/program/program";
 import {ItemProgram} from "../../interfaces/models/item-program";
@@ -11,10 +11,12 @@ export class ProductQuantityComponent implements OnInit {
   @Input() product: Product;
   @Input() quantityFromList;
   @Output() quantityChange = new EventEmitter<any>();
+  @ViewChild('quantityInput') quantityInput:ElementRef;
   public quantity: number = 1;
   public productPrice = 0;
   public total = 0;
   public program: ItemProgram;
+  
 
   constructor(private programProvider: ProgramProvider) {
   }
@@ -23,11 +25,9 @@ export class ProductQuantityComponent implements OnInit {
     this.programProvider.getSelectedProgram().subscribe(program => {
       if (program) {
         this.program = program;
-        if (this.product.QTY_ROUND_OPTION === 'X') {
-          this.quantity = this.quantityFromList ? this.quantityFromList : Number(this.product.SHELF_PACK);
-        }else{
-          this.quantity = this.quantityFromList ? this.quantityFromList : 1
-        }
+   //    this.quantity = this.validateQuantity(this.quantityFromList);
+
+
         this.handleQuantityChange();
         this.productPrice = this.getDecimalPrice();
       }
@@ -78,26 +78,24 @@ export class ProductQuantityComponent implements OnInit {
 
   setPackQuantity(actionType) {
     let selfPackQuantity = Number(this.product.SHELF_PACK);
-    if (this.quantity < selfPackQuantity) {
-      this.quantity = selfPackQuantity;
-    } else {
-      if (this.quantity % selfPackQuantity === 0) {
-        switch (actionType) {
-          case 'ADD':
-            this.quantity = Number(this.quantity) + selfPackQuantity;
-            break;
-          case 'REMOVE':
-            this.quantity = Number(this.quantity) - selfPackQuantity;
-            break;
-        }
-      } else {
-        this.quantity = (selfPackQuantity * Math.floor(this.quantity / selfPackQuantity));
-      }
+    let newQuantity = this.validateQuantity(this.quantity);
+    switch (actionType) {
+      case 'ADD':
+        this.quantity = newQuantity + ((this.product.QTY_ROUND_OPTION === 'X') ? selfPackQuantity : 1);
+        break;
+      case 'REMOVE':
+        this.quantity = newQuantity - ((this.product.QTY_ROUND_OPTION === 'X') ? selfPackQuantity : 1);
+        break;
+      default:
+        this.quantity = newQuantity;
+        break;
     }
   }
 
 
   handleQuantityChange() {
+    this.setPackQuantity("CUSTOM");
+
     this.setTotal();
     let data = {
       quantity: this.quantity,
@@ -105,5 +103,45 @@ export class ProductQuantityComponent implements OnInit {
       productPrice: this.getDecimalPrice()
     };
     this.quantityChange.emit(data);
+  }
+
+  blurInput(){
+    this.quantityInput.nativeElement.blur();
+  }
+
+
+  private getMinimumQuantity(){
+    return Math.max(1,Number(this.program.MINQTY));
+  }
+
+  private validateQuantity(suggestedValue: number){
+
+
+   // console.warn("MINQTY: "+this.program.MINQTY);
+    
+
+    let minQty = this.getMinimumQuantity();
+    if(suggestedValue < minQty){
+      suggestedValue = minQty;
+    }
+
+    if (this.product.QTY_ROUND_OPTION === 'X') {
+      let shelfPack = Number(this.product.SHELF_PACK);
+        if(suggestedValue > shelfPack)
+        {
+          if(shelfPack % suggestedValue === 0){
+            return suggestedValue;
+          }else{
+            return shelfPack * Math.ceil(suggestedValue/shelfPack)
+          }
+        }else
+        {
+          return shelfPack;
+        }
+    }else if(this.product.QTY_ROUND_OPTION === 'Y'){
+      return (suggestedValue)? suggestedValue : minQty;
+    }else{
+      return Math.max(1,suggestedValue);
+    }
   }
 }
