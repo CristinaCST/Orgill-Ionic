c35c629b1a3a65a5ae95774abcb7138b874391fe
ASEBASTIAN <sebastian.axinte@cst-dev.com>
2019-06-20 13:30:33 +0300

    Fix network popover consistency

--- src/services/network/network.ts
+++ src/services/network/network.ts
@@ -6,22 +6,36 @@ import { PopoversService, PopoverContent } from '../popovers/popovers';
 @Injectable()
 export class NetworkService {
 
+  private notified: boolean = false;
+
   constructor(private readonly network: Network, private readonly popoversProvider: PopoversService) {
+
+    if (this.network.type === 'none') {
+      this.openNetworkModal();
+    }
   }
 
   public listenForNetworkEvents(): void {
     this.network.onDisconnect()
       .subscribe(() => {
+        this.notified = false;
+
         this.openNetworkModal();
       });
 
     this.network.onConnect()
       .subscribe(() => {
+        this.notified = false;
         this.popoversProvider.closeModal();
       });
   }
 
   public openNetworkModal(): void {
+    if (this.notified) {
+      return;
+    }
+
+    this.notified = true;
     const content: PopoverContent = this.popoversProvider.setContent(Strings.GENERIC_MODAL_TITLE, Strings.POPOVER_TIMEOUT_ERROR_MESSAGE, 'Open Data', 'Open WIFI');
     this.popoversProvider.show(content);
   }
