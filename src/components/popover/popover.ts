aa8e9ca6f8ed3ba84fe35db06a7b36e7f66f5ace
ASEBASTIAN <sebastian.axinte@cst-dev.com>
2019-07-04 12:52:23 +0300

    Added validation to new custom list popover and indications of issues

--- src/components/popover/popover.ts
+++ src/components/popover/popover.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NavParams, ViewController, Content, NavOptions } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavParams, ViewController, Content, NavOptions, TextInput } from 'ionic-angular';
 import * as Constants from '../../util/constants';
 
 
@@ -14,6 +14,7 @@ export class PopoverComponent {
   public isMarketOnlyList: boolean = false;
   private instantCloseOnNo: boolean = false;
   @ViewChild('immuneElement') private readonly immuneElement: Content;
+  @ViewChild('listNameInput') private readonly listNameElement: TextInput;
 
   constructor(private readonly navParams: NavParams,
               public viewCtrl: ViewController) {
@@ -31,7 +32,7 @@ export class PopoverComponent {
 
   public checkPopoverType(): void {
     if (this.data.type) {
-      this.data.isNewListAlert = this.data.type === Constants.POPOVER_NEW_SHOPPING_LIST;
+      this.data.isNewListModal = this.data.type === Constants.POPOVER_NEW_SHOPPING_LIST;
       this.data.closeOptionAvailable = this.data.type !== Constants.POPOVER_ORDER_CONFIRMATION;
       this.instantCloseOnNo = this.data.type === Constants.POPOVER_QUIT;
     }
@@ -41,15 +42,23 @@ export class PopoverComponent {
     this.isMarketOnlyList = !this.isMarketOnlyList;
   }
 
+  private cleanInput(){
+    this.listNameElement._elementRef.nativeElement.classList.remove('error-label');
+  }
+
   public dismiss(option: string): void {
     const data: any = { type: this.data.type, optionSelected: option };
 
-    if (this.data.isNewListAlert === true) {
+    if (this.data.isNewListModal === true) {
       data.listName = this.listName;
       data.listDescription = this.listDescription;
       data.type = this.isMarketOnlyList ? 'market_only' : 'default';
-    }
 
+      if(this.listName.replace(' ','').length < 1 && option === 'OK'){
+        this.listNameElement._elementRef.nativeElement.classList.add('error-label');
+        return;
+      }
+    }
 
     let navOptions: NavOptions;
     if (this.instantCloseOnNo && option === 'OK') {
