e7765ec7d9fd60863448dc99b52133322c612ce9
Sebastian Axinte <sebastian.axinte.dev@gmail.com>
2019-07-05 19:16:15 +0300

    Don't try to select the same program twice.

--- src/components/product-pricing/product-pricing.ts
+++ src/components/product-pricing/product-pricing.ts
@@ -8,11 +8,11 @@ import { ProgramProvider } from '../../providers/program/program';
   templateUrl: 'product-pricing.html'
 })
 export class ProductPricingComponent implements OnInit {
-  @Input() public isInShoppingList: boolean = false;
+  @Input() public isInShoppingList: boolean = false;  // If the product is viewed from within a shopping list
   @Input() public product: Product;
-  @Input() public productPrograms: ItemProgram[];
-  @Input() public selectedProgram: ItemProgram;
-  public selectedProgramNumber: string;
+  @Input() public productPrograms: ItemProgram[]; // Available programs of the product
+  @Input() public selectedProgram: ItemProgram; // Current program
+  public selectedProgramNumber: string; // Program number of the real-time selected program, used as model for the inputs, do not rely on this if you need old - new values
 
   constructor(private readonly programProvider: ProgramProvider) {}
 
@@ -24,7 +24,9 @@ export class ProductPricingComponent implements OnInit {
   }
 
   public selectProgram(program: ItemProgram): void {
-    this.programProvider.selectProgram(program);
+    if (this.selectedProgram.PROGRAM_NO !== program.PROGRAM_NO) { // Only try to change the program if it's different than the current one
+      this.programProvider.selectProgram(program);
+    }
   }
 
 }
