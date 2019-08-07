4f3a474aab860ac8f2317685749811ddff95779b
ASEBASTIAN <sebastian.axinte@cst-dev.com>
2019-08-07 16:58:32 +0300

    Changed savings formula.

--- src/components/product-quantity/product-quantity.ts
+++ src/components/product-quantity/product-quantity.ts
@@ -111,7 +111,9 @@ export class ProductQuantityComponent implements OnInit {
   }
 
   private setSavings(): void {
-    const percent: number = Math.round((this.regularPrice - this.total / this.quantity) / this.regularPrice * 100);
+    // One day this formula will be very valued again, Cristina wrote it btw.
+    //const percent: number = Math.round((this.regularPrice - this.total / this.quantity) / this.regularPrice * 100);
+    const percent: number = Math.round((this.regularPrice - Number(this.program.PRICE)) / this.regularPrice * 100);
     this.savings = percent > 0 ? percent + '%' : '';
   }
 
