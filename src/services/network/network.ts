78cff0113ebf8558654434e2894a3edfc2068d4a
ASEBASTIAN <sebastian.axinte@cst-dev.com>
2019-08-12 21:02:46 +0300

    Fixed content reloading on first app network change event / check.

--- src/services/network/network.ts
+++ src/services/network/network.ts
@@ -8,14 +8,14 @@ import * as Constants from '../../util/constants';
 @Injectable()
 export class NetworkService {
 
+  private firstNetworkState: boolean = true;  // Needed to avoid initial value call...
+
   public get connectionStatus(): boolean {
     if (this.network.type === 'none' || !this.network.type) {
       return false;
     }
     return true;
   }
-
-  public static firstConnect: boolean = true;
   constructor(private readonly network: Network,
     private readonly popoversService: PopoversService,
     private readonly reloadService: ReloadService,
@@ -23,14 +23,14 @@ export class NetworkService {
 
   public listenForNetworkEvents(): void {
     if (!this.connectionStatus) {
-      NetworkService.firstConnect = false;
       this.openNetworkModal();
     }
 
     this.network.onchange().distinctUntilKeyChanged('type').subscribe(state => {
       if (state.type === 'offline') {
+        this.firstNetworkState = false;
         this.openNetworkModal();
-      } else {
+      } else if (!this.firstNetworkState) { // Needed to avoid initial value call...
         this.popoversService.closeModal(Constants.POPOVER_NETWORK_OFFLINE);
         this.reloadService.announceRetry(true);
       }
