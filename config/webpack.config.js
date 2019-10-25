e6eebcaf84c19fb38dc2d2d6ea898081daa10bab
Saraholu Iustin <iustin.saraholu@cst-dev.com>
2019-10-25 10:26:56 +0300

    environments

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,28 +1,58 @@
-let chalk = require("chalk");
-let fs = require('fs');
-let path = require('path');
-let useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+// let chalk = require("chalk");
+// let fs = require('fs');
+// let path = require('path');
+// let useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
-let env = process.env.IONIC_ENV;
+// let env = process.env.IONIC_ENV;
 
+// useDefaultConfig.prod.resolve.alias = {
+//   "@app/env": path.resolve(environmentPath('prod'))
+// };
+
+// useDefaultConfig.dev.resolve.alias = {
+//   "@app/env": path.resolve(environmentPath('dev'))
+// };
+
+// if (env === 'prod' || env === 'dev') {
+//   // Default to dev config
+//   useDefaultConfig[env] = useDefaultConfig.dev;
+//   useDefaultConfig[env].resolve.alias = {
+//     "@app/env": path.resolve(environmentPath(env))
+//   };
+// }
+
+// function environmentPath(env) {
+//   let filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+//   if (!fs.existsSync(filePath)) {
+//     console.log(chalk.red('\n' + filePath + ' does not exist!'));
+//   } else {
+//     return filePath;
+//   }
+// }
+
+// module.exports = function () {
+//   return useDefaultConfig;
+// };
+
+var chalk = require("chalk");
+var fs = require('fs');
+var path = require('path');
+var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+
+//var env = process.env.IONIC_ENV;
 useDefaultConfig.prod.resolve.alias = {
-  "@app/env": path.resolve(environmentPath('prod'))
+  "@app/env": path.resolve(environmentPath())
 };
 
 useDefaultConfig.dev.resolve.alias = {
-  "@app/env": path.resolve(environmentPath('dev'))
+  "@app/env": path.resolve(environmentPath())
 };
 
-if (env === 'prod' || env === 'dev') {
-  // Default to dev config
-  useDefaultConfig[env] = useDefaultConfig.dev;
-  useDefaultConfig[env].resolve.alias = {
-    "@app/env": path.resolve(environmentPath(env))
-  };
-}
-
 function environmentPath(env) {
-  let filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+  //var filePath = './src/env/env' + (env === 'prod' ? '.' + env : '') + '.ts';
+
+  var filePath = './src/environments/environment.dev.ts';
+
   if (!fs.existsSync(filePath)) {
     console.log(chalk.red('\n' + filePath + ' does not exist!'));
   } else {
@@ -30,6 +60,18 @@ function environmentPath(env) {
   }
 }
 
+function consoleOut(env) {
+  console.log("=============== ENVIRONMENT ===============");
+  console.log("                                           ");
+  console.log("                 "+env+"                   ");
+  console.log("                                           ");
+  console.log("===========================================");
+}
+
 module.exports = function () {
+
+  var env = process.env.ENV || 'dev';
+
+  consoleOut(env);
   return useDefaultConfig;
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { URL_BASE_PROD } from '../util/constants-url';
 export const environment: { production: boolean, baseUrlEnglish: string, baseUrlFrench: string } = {
-  production: true,
+  production: false,
   baseUrlEnglish: URL_BASE_PROD,
-  baseUrlFrench: 'http://dmwebservice-cafr.orgill.com/service.asmx'
+  baseUrlFrench: 'http://reststage.orgill.com/service.asmx'
 };
