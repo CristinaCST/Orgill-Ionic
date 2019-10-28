e4fd424f938aa79851061696e0a758c5e76066e3
Saraholu Iustin <iustin.saraholu@cst-dev.com>
2019-10-28 10:28:34 +0200

    environments v2

--- config/environment-script.js
+++ config/environment-script.js
@@ -1,16 +1,18 @@
 var fs = require('fs');
 
 function readWriteSync() {
-  let env = process.env.ENV || 'dev';
+  let env = process.env.ENV || 'prod';
+  console.log('ENVVNVNVNVN', env)
 
   var data = fs.readFileSync(`src/environments/environment.${env}.ts`, 'utf-8');
   fs.writeFileSync('src/environments/environment.ts', data, 'utf-8');
+  console.log('DATAAA', data)
   
   consoleOut();
 }
 
 function consoleOut() {
-  let env = process.env.ENV || 'dev';
+  let env = process.env.ENV || 'prod';
   let filePath = `src/environments/environment.${env}.ts`;
   
   console.log("============ PREPARE ENV FILE =============");
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,58 +1,28 @@
-// let chalk = require("chalk");
-// let fs = require('fs');
-// let path = require('path');
-// let useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+let chalk = require("chalk");
+let fs = require('fs');
+let path = require('path');
+let useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
-// let env = process.env.IONIC_ENV;
+let env = process.env.IONIC_ENV;
 
-// useDefaultConfig.prod.resolve.alias = {
-//   "@app/env": path.resolve(environmentPath('prod'))
-// };
-
-// useDefaultConfig.dev.resolve.alias = {
-//   "@app/env": path.resolve(environmentPath('dev'))
-// };
-
-// if (env === 'prod' || env === 'dev') {
-//   // Default to dev config
-//   useDefaultConfig[env] = useDefaultConfig.dev;
-//   useDefaultConfig[env].resolve.alias = {
-//     "@app/env": path.resolve(environmentPath(env))
-//   };
-// }
-
-// function environmentPath(env) {
-//   let filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
-//   if (!fs.existsSync(filePath)) {
-//     console.log(chalk.red('\n' + filePath + ' does not exist!'));
-//   } else {
-//     return filePath;
-//   }
-// }
-
-// module.exports = function () {
-//   return useDefaultConfig;
-// };
-
-var chalk = require("chalk");
-var fs = require('fs');
-var path = require('path');
-var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
-
-//var env = process.env.IONIC_ENV;
 useDefaultConfig.prod.resolve.alias = {
-  "@app/env": path.resolve(environmentPath())
+  "@app/env": path.resolve(environmentPath('prod'))
 };
 
 useDefaultConfig.dev.resolve.alias = {
-  "@app/env": path.resolve(environmentPath())
+  "@app/env": path.resolve(environmentPath('dev'))
 };
 
+if (env === 'prod' || env === 'dev') {
+  // Default to dev config
+  useDefaultConfig[env] = useDefaultConfig.dev;
+  useDefaultConfig[env].resolve.alias = {
+    "@app/env": path.resolve(environmentPath(env))
+  };
+}
+
 function environmentPath(env) {
-  //var filePath = './src/env/env' + (env === 'prod' ? '.' + env : '') + '.ts';
-
-  var filePath = './src/environments/environment.dev.ts';
-
+  let filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
   if (!fs.existsSync(filePath)) {
     console.log(chalk.red('\n' + filePath + ' does not exist!'));
   } else {
@@ -60,18 +30,48 @@ function environmentPath(env) {
   }
 }
 
-function consoleOut(env) {
-  console.log("=============== ENVIRONMENT ===============");
-  console.log("                                           ");
-  console.log("                 "+env+"                   ");
-  console.log("                                           ");
-  console.log("===========================================");
-}
-
 module.exports = function () {
-
-  var env = process.env.ENV || 'dev';
-
-  consoleOut(env);
   return useDefaultConfig;
 };
+
+// var chalk = require("chalk");
+// var fs = require('fs');
+// var path = require('path');
+// var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+
+// //var env = process.env.IONIC_ENV;
+// useDefaultConfig.prod.resolve.alias = {
+//   "@app/env": path.resolve(environmentPath())
+// };
+
+// useDefaultConfig.dev.resolve.alias = {
+//   "@app/env": path.resolve(environmentPath())
+// };
+
+// function environmentPath(env) {
+//   //var filePath = './src/env/env' + (env === 'prod' ? '.' + env : '') + '.ts';
+
+//   var filePath = './src/environments/environment.dev.ts';
+
+//   if (!fs.existsSync(filePath)) {
+//     console.log(chalk.red('\n' + filePath + ' does not exist!'));
+//   } else {
+//     return filePath;
+//   }
+// }
+
+// function consoleOut(env) {
+//   console.log("=============== ENVIRONMENT ===============");
+//   console.log("                                           ");
+//   console.log("                 "+env+"                   ");
+//   console.log("                                           ");
+//   console.log("===========================================");
+// }
+
+// module.exports = function () {
+
+//   var env = process.env.ENV || 'dev';
+
+//   consoleOut(env);
+//   return useDefaultConfig;
+// };
