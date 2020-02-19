858a2f1e92a8aa657760b16e6dc4af140caced1f
Marius Cimpoeru <marius.cimpoeru@cst.ro>
2020-02-19 20:29:18 +0200

    allow language switch only for divisio 8

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as VersionFile from '../../util/version';
 import { OneSignalService } from '../../services/onesignal/onesignal';
 import { ModalController, Modal } from 'ionic-angular';
+import { AuthService } from '../../services/auth/auth';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit {
   public applicationVersion: String = VersionFile.VERSION;
   public notificationsAllowed: boolean = false;
+  public allowLanguageSwitch: boolean;
 
   constructor(
     private readonly oneSignalService: OneSignalService,
-    private readonly modal: ModalController
+    private readonly modal: ModalController,
+    private readonly authService: AuthService
   ) {
     this.oneSignalService.isSubscriptionOn().then(state => this.notificationsAllowed = state);
+  }
+
+  public ngOnInit(): void {
+    this.authService.getAllowLanguageSwitchListener().subscribe(result => {
+      this.allowLanguageSwitch = result;
+    });
   }
 
   public pushNotificationsSwitch(event: { checked: any; }): void {
@@ -26,9 +35,10 @@ export class SettingsPage {
       }
   }
 
+
+
   public openModal(): void {
     const modalLanguages: Modal = this.modal.create('ModalLanguagesPage');
-
     modalLanguages.present();
   }
 }
--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -6,7 +6,7 @@ import * as ConstantsURL from '../../util/constants-url';
 import { LocalStorageHelper } from '../../helpers/local-storage';
 import * as Constants from '../../util/constants';
 import { User } from '../../interfaces/models/user';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Events } from 'ionic-angular';
 import { Moment } from 'moment';
 import { DateTimeService } from '../../services/datetime/dateTimeService';
@@ -18,12 +18,14 @@ import { PopoversService, PopoverContent } from '../../services/popovers/popover
 export class AuthService {
 
   private user: User = new User();
+  public allowSwitch: string;
+  public allowLanguageSwitch: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private readonly apiProvider: ApiService,
               private readonly events: Events,
               private readonly secureActions: SecureActionsService,
               private readonly popoverService: PopoversService) {
-    
+
       this.secureActions.authState.subscribe(authOk => {
       if (authOk) {
         this.events.publish(Constants.EVENT_AUTH);
@@ -42,6 +44,10 @@ export class AuthService {
   private get User(): User {
     return this.user;
   }
+
+  public getAllowLanguageSwitchListener(): Observable<boolean> {
+    return this.allowLanguageSwitch.asObservable();
+}
 
   public login(credentials: LoginRequest): Observable<void> {
 
@@ -69,6 +75,8 @@ export class AuthService {
         this.apiProvider.post(ConstantsURL.URL_USER_INFO, params).subscribe(response => {
           this.user = JSON.parse(response.d);
           this.user.userToken = params.user_token;
+          this.allowSwitch = JSON.parse(response.d).division;
+          this.allowLanguageSwitch.next(this.allowSwitch === '8');
           this.secureActions.setAuthState(true, this.user);
           this.events.publish(Constants.EVENT_AUTH);
           LocalStorageHelper.saveToLocalStorage(Constants.USER, JSON.stringify(this.user));
@@ -103,7 +111,7 @@ export class AuthService {
     this.popoverService.show(content);
 
     this.logout(true);
-    
+
   }
 
 }
