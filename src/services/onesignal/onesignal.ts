ffb11278285d6adc2c1e2bedf8d7c23c62a45612
Marius Cimpoeru <marius.cimpoeru@cst.ro>
2020-03-04 11:29:31 +0200

    oneSignal.setLogLevel setup

--- src/services/onesignal/onesignal.ts
+++ src/services/onesignal/onesignal.ts
@@ -93,8 +93,7 @@ export class OneSignalService {
 
         // Set the logging level of OneSignal
         this.oneSignal.setLogLevel({
-         // logLevel: Constants.ONE_SIGNAL_VERBOSE ? Constants.ONE_SIGNAL_DEBUG_LEVEL : 1,
-            logLevel: 6, // Are we verbose logging by choice? If not, log only fatal errors
+            logLevel: Constants.ONE_SIGNAL_VERBOSE ? Constants.ONE_SIGNAL_DEBUG_LEVEL : 1,
             visualLevel: 0 // Never pop up error as alerts with the built-in system.
         });
 
