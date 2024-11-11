c84f814692fc9887cc28d60929687045b9dcaccd
Cristina Nicolae <cristina@cst.ro>
2024-11-11 18:38:46 +0200

    Preparing november 2024 release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { URL_BASE_PROD, URL_BASE_PROD_NEW } from '../util/constants-url';
 export const environment: { production: boolean; baseUrlEnglish: string; baseUrlFrench: string } = {
   production: true,
   baseUrlEnglish: URL_BASE_PROD_NEW,
-  baseUrlFrench: 'http://reststage-cafr.orgill.com/api/v1/'
+  baseUrlFrench: 'https://ozoneapi-cafr.orgill.com/api/v1/'
 };
